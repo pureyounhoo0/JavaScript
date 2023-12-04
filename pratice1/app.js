b6af2fc3c15037708bb6db3b8d51c5a413c1ebf9
나윤후 <younhoo070603@naver.com>
2023-12-04 16:12:43 +0900

    pratice1 end commit

--- pratice1/app.js
+++ pratice1/app.js
@@ -1,8 +1,29 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button")
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
-    console.log("hello",loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+   event.preventDefault();
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   const username = loginInput.value;
+   localStorage.setItem(USERNAME_KEY,username);
+  paintGreetings(username);
 }
 
-loginButton.addEventListener("click",onLoginBtnClick)
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){//form이 보여짐
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{//greeting이 보여짐
+   paintGreetings(savedUsername);
+}
